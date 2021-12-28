2479b7fc929d5a262185369fc213e7f312c8f450
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-28 23:37:07 +0100

    vuex state management

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,25 @@
   <base-container title="Vuex">
     <add-counter></add-counter>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
-import AddCounter from './components/AddCounter.vue'
+import AddCounter from './components/AddCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     AddCounter,
+    ChangeCounter
   },
 
   methods: {
     addOne() {
-          this.$store.state.counter++;
+      this.$store.commit('increment');
     }
   }
 };

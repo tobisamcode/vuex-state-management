b5cf00697091b0cbb31af71dc17d82db7a29797e
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-29 00:06:33 +0100

    vuex state management

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ const store = createStore({
     mutations: {
         increment(state) {
             state.counter = state.counter + 2;
+        },
+        increase(state, payload) {
+            state.counter = state.counter + payload.value;
         }
     }
 });
